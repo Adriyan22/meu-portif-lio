5dab60a180b0f45a86b45189c1fd7b0c260d0425
Adriyan Web Developer <145672511+Adriyan22@users.noreply.github.com>
2024-12-14 05:25:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let menuLista = document.querySelector('.nav-lista')
+let btn = document.querySelector('.anterior')
+let page = document.querySelector('.sobre')
+
+function exibir(){
+    menuLista.classList.toggle('nav-lista')
+    menuLista.classList.toggle('menu-mobile')
+    menuLista.classList.toggle('li')
+}
+
---
+++ sobre.js
@@ -0,0 +1,54 @@
+let container = document.querySelector('.container-md')
+
+let div = document.querySelector('.btn')
+let btnAnterior = document.querySelector('.anterior')
+let btnProxima = document.querySelector('.proxima')
+
+container = document.createElement('img')
+container.src = './img/perfil.png'
+container.style.position = 'absolute'
+container.style.top = '0'
+container.style.marginTop = '1em'
+container.style.borderRadius = '0 20px'
+container.style.boxShadow = '3px 3px 10px #ffffff5d'
+document.body.appendChild(container)
+
+
+div.addEventListener('mouseenter', () => {
+    btnAnterior.style.backgroundColor = '#99cd3294'
+    btnProxima.style.backgroundColor = '#99cd3294'
+    div.style.border = 'none'
+})
+
+btnAnterior.addEventListener('click', () => {
+    btnAnterior.style.scale = '1.05'
+    btnAnterior.style.color = '#fff'
+
+    if(btnAnterior != btnProxima){
+        btnProxima.style.scale = '1.0'
+        btnProxima.style.color = '#000000'
+        btnProxima.style.backgroundColor = '#99cd3294'
+        btnAnterior.style.backgroundColor = '#9acd32'
+        btnAnterior.style.textDecoration = 'underline'
+    }
+
+    btnAnterior.addEventListener('mouseout', ()=> {
+        btnAnterior.style.textDecoration = 'none'
+    })
+})
+
+btnProxima.addEventListener('click', () => {
+    btnProxima.style.scale = '1.05'
+    btnProxima.style.color = '#fff'
+    btnProxima.style.backgroundColor = '#9acd32'
+    if(btnProxima != btnAnterior){
+        btnAnterior.style.color = 'black'
+        btnAnterior.style.scale = '1.0'
+        btnAnterior.style.backgroundColor = '#99cd3294'
+        btnProxima.style.textDecoration = 'underline'
+    }
+
+    btnProxima.addEventListener('mouseout', ()=> {
+        btnProxima.style.textDecoration = 'none'
+    })
+})
